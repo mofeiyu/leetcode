b3fb491bee59808c5357b7007158116089240c19
mofeiyu <mofeiyu1110@foxmail.com>
2017-07-20 00:03:50 +0800

    leetcode002

---
+++ 002_add_two_numbers.py
@@ -0,0 +1,22 @@
+tion for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        carry = 0
+        l3 = ListNode(0)
+        p = l3
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
+            carry, val = divmod(carry, 10)
+            p.next = ListNode(val)
+            p = p.next
+        return l3.next
