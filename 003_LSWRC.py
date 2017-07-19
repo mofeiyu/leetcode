eb46571bb6a906e8eb76707c3211e22f35238534
mofeiyu <mofeiyu1110@foxmail.com>
2017-07-20 00:07:02 +0800

    leetcode class Solution(object):

---
+++ 003_LSWRC.py
@@ -0,0 +1,17 @@
+ass Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return process(s)
+def process(text):
+    ans = 0
+    pos_dict = {}
+    lpos = 0
+    for i, ch in enumerate(text):
+        if ch in pos_dict:
+            lpos = pos_dict[ch] + 1
+        pos_dict[ch] = i
+        ans = max(ans, i - lpos + 1)
+    return ans
